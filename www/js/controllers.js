868a7c708d113ce8037aace2722f0dfd270998ce
BabyManisha <babymaneesha@gmail.com>
2016-11-04 07:46:31 +0530

    included angularjs

---
+++ www/js/controllers.js
@@ -0,0 +1,5 @@
+angular.module('deals.controllers', [])
+
+    .controller('DealsLoginCtrl', function ($scope, $stateParams) {})
+
+    .controller('DealsHomeCtrl', function ($scope, $stateParams) {});
